08feaaa68ba864e8e1beaa80316d8654260f30c9
stefan-lonac <62054085+stefan-lonac@users.noreply.github.com>
2022-02-08 08:49:25 +0100

    Slider animation update

--- module.js
+++ module.js
@@ -1,96 +1,93 @@
-window.onload = function() {
 
-    // *** Image slider ***
+    const slider_id = document.querySelectorAll('.slider-container');
 
-    // Variables
-    slides          = Array.from(document.querySelectorAll('.slide-item')),
-    sliders_content = Array.from(document.querySelectorAll('.slide-item .content')),
-    slider_id       = document.getElementById('slider-container'),
-    next_slide_id   = document.getElementById('next-slide'),
-    prev_slide_id   = document.getElementById('prev-slide'),
-    autoplay        = slider_id.getAttribute('data-autostart'),
-    pause_hover     = slider_id.getAttribute('data-pause'),
-    timer_slider    = slider_id.getAttribute('data-timer'),
-    index_slide     = 1,
-    time = timer_slider;
-
-    // Start Autoplay slider if change to True
-    if (autoplay == 'true') {
-
-        // On page load start Autoplay
-        ShowSlides(index_slide)
-        let autoPlaySet = setInterval(function() { ShowSlides(index_slide++); }, time);
-
-        // If set Hover to TRUE 
-        if (pause_hover == 'true') {
-
-            // On hover stop Autoplay
-            slider_id.onmouseover = function() {
-                autoPlaySet = clearInterval(autoPlaySet);
-            }
-
-            // On hover out start again Autoplay
-            slider_id.onmouseout = function() {
-                autoPlaySet = setInterval(function() { ShowSlides(index_slide++); }, time);
-            }
-
-        } 
+    for( let i = 0; i < slider_id.length; i++ ) {
         
-    } else {
-        ShowSlides();
-    }
-    
-    // Next slide click
-    next_slide_id.addEventListener('click', function() {
-        ShowSlides(index_slide += 1);
-    });
+        const next_slide_id = slider_id[i].querySelector('.next-slide');
+        const prev_slide_id = slider_id[i].querySelector('.prev-slide');
+        const autoplay = slider_id[i].getAttribute('data-autostart');
+        const pause_hover = slider_id[i].getAttribute('data-pause');
+        const timer_slider = slider_id[i].getAttribute('data-timer');
+        let time = timer_slider;
+        let index_slide = 0;
 
-    // Previous slide click
-    prev_slide_id.addEventListener('click', function() {
-        ShowSlides(index_slide -= 1);
-    });
+        // Show Slider
+        const ShowSlides = function(n) {
+
+            const slides = slider_id[i].querySelectorAll('.slide-item');
+            const sliders_content = slider_id[i].querySelectorAll('.slide-item .content');
+
+            const slides_loop = function(s, x) {
+                s.style.transform = `translateX(${100 * (index_slide - x)}%)`;
+                s.removeAttribute('data-item-slide');
+                s.classList.remove("active-slide");
+            }
+
+            const active_slides = function () {
+                slides[index_slide].setAttribute('data-item-slide', 'active');  
+                slides[index_slide].classList.add('active-slide');
+            }
 
 
-    // Show Slider
-    function ShowSlides(n) {
+            // Start Autoplay slider if change to True
+            if (autoplay == 'true') {
+  
+                if (n < 0) { index_slide = slides.length - 1; }
+                
+                if (n >= slides.length - 1) { index_slide = 0; }
+                
+                slides.forEach(slides_loop);
+                active_slides();
 
-        // Loop through all sliders and hides that are not active
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-            slides[i].removeAttribute('data-item-slide');
+            } else {
+                
+                if (n >= slides.length) { index_slide = 0; }
+                
+                if (n < 0) { index_slide = slides.length - 1; }
+                
+                slides.forEach(slides_loop);
+                active_slides();
+            }
+                
         }
 
         // Start Autoplay slider if change to True
         if (autoplay == 'true') {
 
-                if (n < 1) {
-                    index_slide = slides.length;
-                    console.log('hi');
+            // On page load start Autoplay
+            ShowSlides(index_slide)
+            let autoPlaySet = setInterval(function() { ShowSlides(index_slide++); }, time);
+
+            // If set Hover to TRUE 
+            if (pause_hover == 'true') {
+
+                // On hover stop Autoplay
+                slider_id.onmouseover = function() {
+                    autoPlaySet = clearInterval(autoPlaySet);
                 }
 
-                if (n >= slides.length) {
-                    index_slide = 1;
+                // On hover out start again Autoplay
+                slider_id.onmouseout = function() {
+                    autoPlaySet = setInterval(function() { ShowSlides(index_slide++); }, time);
                 }
 
-                slides[index_slide - 1].style.display = "block";
-                slides[index_slide - 1].setAttribute('data-item-slide', 'active');            
-
+            } 
+            
         } else {
-            
-            if (n > slides.length) {
-                index_slide = 1;
-            }
-            
-            if (n < 1) {
-                index_slide = slides.length;
-            }
-
-            slides[index_slide - 1].style.display = "block";
-            slides[index_slide - 1].setAttribute('data-item-slide', 'active');
-
+            ShowSlides();
         }
-            
-    }
-    // *** END: Image slider ***
+        
 
-}
+        // Next slide click
+        next_slide_id.addEventListener('click', function() {
+            ShowSlides(index_slide += 1);
+        });
+
+        // Previous slide click
+        prev_slide_id.addEventListener('click', function() {
+            ShowSlides(index_slide -= 1);
+        });
+
+
+    }
+
